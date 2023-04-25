b9720f1c9f092af8445b84b6009c4b4699618e48
Kichan <rlcks01537@gmail.com>
2023-04-25 16:23:14 +0900

    FullTube userSchema exists

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -5,7 +5,21 @@ export const getJoin = (req, res) => {
 };
 
 export const postJoin = async (req, res) => {
-  const { email, name, password, username } = req.body;
+  const { email, name, password, password2, username } = req.body;
+  const exist = await userModel.exists({ $or: [{ username }, { email }] });
+  const pageTitle = "회원 가입";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "비밀번호를 틀렸습니다. 재입력 해주세요.",
+    });
+  }
+  if (exist) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "이미 사용 중인 E-mail 또는 닉네임 입니다.",
+    });
+  }
   await userModel.create({
     email,
     name,
--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   password: { type: String, required: true },
   username: { type: String, required: true, unique: true },
 });

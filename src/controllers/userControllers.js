ed0cf5a2bb74bbf2543b9a69356cce13563e9d5d
Kichan <rlcks01537@gmail.com>
2023-04-26 13:14:02 +0900

    FullTube Login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import userModel from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "회원 가입" });
@@ -6,31 +7,58 @@ export const getJoin = (req, res) => {
 
 export const postJoin = async (req, res) => {
   const { email, name, password, password2, username } = req.body;
-  const exist = await userModel.exists({ $or: [{ username }, { email }] });
   const pageTitle = "회원 가입";
   if (password !== password2) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "비밀번호를 틀렸습니다. 재입력 해주세요.",
     });
   }
+  const exist = await userModel.exists({ $or: [{ username }, { email }] });
   if (exist) {
     return res.status(400).render("join", {
       pageTitle,
       errorMessage: "이미 사용 중인 E-mail 또는 닉네임 입니다.",
     });
   }
-  await userModel.create({
-    email,
-    name,
-    password,
-    username,
-  });
+  try {
+    await userModel.create({
+      email,
+      name,
+      password,
+      username,
+    });
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
   return res.redirect("/login");
 };
 
-export const loginUser = (req, res) => {
-  return res.render("login", { pageTitle: "Login" });
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: `로그인` });
+};
+
+export const postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  const pageTitle = "로그인";
+  if (!user) {
+    res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Email이 일치 하지 않습니다.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "비밀번호가 일치 하지 않습니다.",
+    });
+  }
+  return res.redirect("/");
 };
 
 export const editUser = (req, res) => {
--- src/routers/homeRouter.js
+++ src/routers/homeRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { getJoin, postJoin, loginUser } from "../controllers/userControllers";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userControllers";
 import { homeVideo, searchVideo } from "../controllers/videoControllers";
 
 const homeRouter = express.Router();
 
 homeRouter.get("/", homeVideo); //video Controller(main Video)
-homeRouter.get("/login", loginUser);
 homeRouter.route("/join").get(getJoin).post(postJoin);
+homeRouter.route("/login").get(getLogin).post(postLogin);
 homeRouter.get("/search", searchVideo);
 //user Controller(join User)
 //only Logged In user or inside Log In user are +URL /user/

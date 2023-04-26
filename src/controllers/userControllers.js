bcfd110672f648cebc890683b71a2d0355792761
Kichan <rlcks01537@gmail.com>
2023-04-26 15:46:22 +0900

    FullTube session & Login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -58,6 +58,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "비밀번호가 일치 하지 않습니다.",
     });
   }
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import homeRouter from "./routers/homeRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -11,6 +13,16 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Log",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", homeRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);

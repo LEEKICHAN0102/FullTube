45d8dc0ded30b1058a103ad35deaebbe7d99a5bc
Kichan <rlcks01537@gmail.com>
2023-04-27 16:37:34 +0900

    FullTube Just Recap-Next day code authentication

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,5 +2,8 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "FullTube";
   res.locals.loggedInUser = req.session.user;
+  console.log(res.locals);
   next();
 };
+
+//Template 에서 session의 값을 받아 올 수 없기에 locals로 그 값을 받아서 유저가 로그인 하였는지 등의 Object를 확인 할 수 있는 것.
